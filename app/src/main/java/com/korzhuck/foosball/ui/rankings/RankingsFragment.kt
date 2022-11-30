8900c591ea6886ae499de03f391246efd03e4f6b
Siarhei Serdziukou <freemolto@gmail.com>
2022-11-30 17:55:21 +0100

    GH-6 added sort icon to action bar

--- app/src/main/java/com/korzhuck/foosball/ui/rankings/RankingsFragment.kt
+++ app/src/main/java/com/korzhuck/foosball/ui/rankings/RankingsFragment.kt
@@ -2,11 +2,16 @@ package com.korzhuck.foosball.ui.rankings
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
+import com.korzhuck.foosball.R
 import com.korzhuck.foosball.databinding.FragmentRankingsBinding
 import com.korzhuck.foosball.domain.usecase.SortOrder
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +25,19 @@ class RankingsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val menuProvider = object : MenuProvider {
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            menuInflater.inflate(R.menu.ab_rankings, menu)
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+            when (menuItem.itemId) {
+                R.id.sort -> {
+                    true
+                }
+                else -> false
+            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,12 +45,14 @@ class RankingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRankingsBinding.inflate(inflater, container, false)
+        requireActivity().addMenuProvider(menuProvider)
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        requireActivity().removeMenuProvider(menuProvider)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
